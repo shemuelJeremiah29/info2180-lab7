4168c30495068180bd0d082e7f22911fb6d08130
Andrew Morris <andrew.morris03@mymona.uwi.edu>
2017-11-17 15:23:38 +0000

    Exercise 3

---
+++ world.js
@@ -0,0 +1,23 @@
+window.onload= function (){ 
+    var clickable=document.getElementById("lookup"); 
+   
+                function clickButton(){ 
+                 clickable.addEventListener("click", function(){ 
+                     var http = new XMLHttpRequest();   
+                     http.onreadystatechange=function(){  
+                         if (this.readyState == 4 && this.status == 200) {  
+                             console.log(http.responseText);
+                             alert(http.responseText); 
+                            document.getElementById("result").innerHTML= http.responseText;
+                             
+                         }
+                     };   
+                     var docval= document.getElementById('country').value; 
+                    //  var url ="https://ide.c9.io/shemueljeremiah21/info2180_lab7/world.php";
+                     http.open('GET',"world.php?country=Jamaica",true);  
+                     http.send(null); 
+                 });
+                
+    }
+    clickButton(); 
+}
